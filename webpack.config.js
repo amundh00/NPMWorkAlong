59bd92c0955c9fcafa54552ec61a7a18a509a0cc
Amund Halgunset <amundhalgunset@gmail.com>
2024-09-04 13:48:19 +0200

    damn

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const htmlWebpackPlugin = require("html-webpack-plugin"); 
+module.exports = { 
+    mode: 'production', 
+    module: { 
+        rules: [ 
+            { 
+                test: /\.html$/, 
+                use: [ 
+                    { 
+                        loader: "html-loader", 
+                        options: { minimize: true }
+                    } 
+                ] 
+            }
+        ]
+    },
+    devServer: { 
+        hot: false, // disable HMR 
+        open: { 
+            app: { 
+                name: 'Google Chrome',
+            } 
+        } 
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+        template: './src/index.html', 
+        filename: './index.html' 
+    }) 
+    ] 
+}
